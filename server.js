5ca8f19e2e5ab50ccd4f55b665c03df0c63328b0
venom <eyueldaniel0921@gmail.com>
2022-12-14 21:20:52 +0300

    init commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const { Telegraf } = require('telegraf');
+const { message } = require('telegraf/filters');
+require('dotenv').config();
+
+console.log(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
